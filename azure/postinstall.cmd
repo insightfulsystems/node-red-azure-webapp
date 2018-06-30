b0bd82179d8244b2ead6552d04d8a2dba6ec5a63
Jean-Philippe Encausse <Jp@encausse.net>
2018-07-01 00:19:05 +0200

    Fix script path

--- azure/postinstall.cmd
+++ azure/postinstall.cmd
@@ -1,8 +1,13 @@
 (if not exist \home mkdir \home)
 cd \home
+
+(if exist \home\viseo-bot-framework goto :endofscript)
 curl -L https://github.com/NGRP/node-red-viseo-bot/archive/master.zip --output framework.zip
 unzip framework.zip
 rm framework.zip
 mv node-red-viseo-bot-master viseo-bot-framework
 cd \home\viseo-bot-framework
 npm install
+
+:endofscript
+echo "Script complete"
