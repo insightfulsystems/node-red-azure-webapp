ee444c0c51286b5583b7b04f8a0c99163e126a6c
Jean-Philippe Encausse <Jp@encausse.net>
2018-07-01 17:13:24 +0200

    require the d:/ for windows path

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 process.env.PORT              = process.env.PORT              || 8000
 process.env.HOST              = process.env.HOST              || "https://localhost"
 process.env.NODE_ENV          = process.env.NODE_ENV          || "dev"
-process.env.FRAMEWORK_ROOT    = process.env.FRAMEWORK_ROOT    || "/home/site/viseo-bot-framework"
-process.env.BOT_ROOT          = process.env.BOT_ROOT          || "/home/site/wwwroot"
+process.env.FRAMEWORK_ROOT    = process.env.FRAMEWORK_ROOT    || "D:/home/site/viseo-bot-framework"
+process.env.BOT_ROOT          = process.env.BOT_ROOT          || "D:/home/site/wwwroot"
 process.env.CONFIG_PATH       = process.env.CONFIG_PATH       || (process.env.BOT_ROOT+"/conf/config.js")
 process.env.CREDENTIAL_SECRET = process.env.CREDENTIAL_SECRET || "your secret here"
 
