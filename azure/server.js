a348cd14cd93a12cd3c53ff9af7b1edba129b968
Jean-Philippe Encausse <Jp@encausse.net>
2018-07-01 14:43:21 +0200

    Call red.js with require and arguments

--- azure/server.js
+++ azure/server.js
@@ -1,9 +1,4 @@
-const http    = require('http');
-const express = require("express");
-const RED     = require('node-red');
-const app     = express();
-
-process.env.PORT              = process.env.PORT              || 1880
+process.env.PORT              = process.env.PORT              || 8000
 process.env.HOST              = process.env.HOST              || "https://localhost"
 process.env.NODE_ENV          = process.env.NODE_ENV          || "dev"
 process.env.FRAMEWORK_ROOT    = process.env.FRAMEWORK_ROOT    || "/home/viseo-bot-framework"
@@ -11,14 +6,6 @@ process.env.BOT_ROOT          = process.env.BOT_ROOT          || "/home/site/www
 process.env.CONFIG_PATH       = process.env.CONFIG_PATH       || (process.env.BOT_ROOT+"/conf/config.js")
 process.env.CREDENTIAL_SECRET = process.env.CREDENTIAL_SECRET || "your secret here"
 
-const server    = http.createServer(app);
-const settings  = require(process.env.FRAMEWORK_ROOT+"/conf/node-red-config.js");
-
-RED.init(server, settings);
-
-app.use(settings.httpAdminRoot,RED.httpAdmin);
-app.use(settings.httpNodeRoot,RED.httpNode);
- 
-server.listen(settings.uiPort);
-console.log(`listening port:${settings.uiPort}`);
-RED.start();
+process.argv.push("-s")
+process.argv.push(process.env.FRAMEWORK_ROOT +"/conf/node-red-config.js")
+require(process.env.FRAMEWORK_ROOT+'/node_modules/node-red/red.js');
