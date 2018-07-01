4a9b72ec4da2826d265654d89ef8f52026b9344c
Jean-Philippe Encausse <Jp@encausse.net>
2018-07-01 16:13:26 +0200

    fix install theme path

--- azure/postinstall.cmd
+++ azure/postinstall.cmd
@@ -1,12 +1,15 @@
 (if not exist \home mkdir \home)
 cd \home
 
-(if exist \home\viseo-bot-framework goto :endofscript)
+(if not exist \home\site mkdir \home\site)
+cd \home\site
+
+(if exist \home\site\viseo-bot-framework goto :endofscript)
 curl -L https://github.com/NGRP/node-red-viseo-bot/archive/master.zip --output framework.zip
 unzip framework.zip
 rm framework.zip
 mv node-red-viseo-bot-master viseo-bot-framework
-cd \home\viseo-bot-framework
+cd \home\site\viseo-bot-framework
 npm install
 
 :endofscript
--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 process.env.PORT              = process.env.PORT              || 8000
 process.env.HOST              = process.env.HOST              || "https://localhost"
 process.env.NODE_ENV          = process.env.NODE_ENV          || "dev"
-process.env.FRAMEWORK_ROOT    = process.env.FRAMEWORK_ROOT    || "/home/viseo-bot-framework"
+process.env.FRAMEWORK_ROOT    = process.env.FRAMEWORK_ROOT    || "/home/site/viseo-bot-framework"
 process.env.BOT_ROOT          = process.env.BOT_ROOT          || "/home/site/wwwroot"
 process.env.CONFIG_PATH       = process.env.CONFIG_PATH       || (process.env.BOT_ROOT+"/conf/config.js")
 process.env.CREDENTIAL_SECRET = process.env.CREDENTIAL_SECRET || "your secret here"
 
 process.argv.push("-s")
 process.argv.push(process.env.FRAMEWORK_ROOT +"/conf/node-red-config.js")
-
-const fs = require('fs')
-fs.writeFileSync(process.env.BOT_ROOT+'/debug_jp.txt','Starting red.js')
-console.log('Starting red.js');
 
 require(process.env.FRAMEWORK_ROOT+'/node_modules/node-red/red.js')
